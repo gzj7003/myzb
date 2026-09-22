b88723b5e0e19372de5d2ffc150e67b361e0cce8
gzj7003 <gzj7003@163.com>
2026-09-22 22:50:42 +0800

    Update URL in items-hj.py

--- set/items-hj.py
+++ set/items-hj.py
@@ -41,7 +41,7 @@ def filter_live_sources():
     ]
     
     urls = [
-        "https://raw.githubusercontent.com/gzj7003/iptvz/refs/heads/main/zubo.txt",
+        "https://raw.githubusercontent.com/gzj7003/tv/refs/heads/main/itvlist.txt",
         "https://raw.githubusercontent.com/gzj7003/iptvz/refs/heads/main/txt/%E6%B9%96%E5%8D%97%E7%94%B5%E4%BF%A1.txt"
     ]
     
